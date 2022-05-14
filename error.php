17a0ccaa1e2eaf73a3210737ec5e7f1ec6e3dc93
shahr6485 <1903166.cse.cec@cgc.edu.in>
2022-05-14 14:26:34 +0530

    Create error.php

---
+++ error.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <!-- Required meta tags -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <!-- CSS File -->
+  <link rel="stylesheet" href="style.css">
+  <title>Mail Verify</title>
+</head>
+
+<body>
+
+  
+
+  <section class="inline">
+    <div class="content">
+      <header>
+        <h1>Email not sent!</h1>
+      </header>
+      <section>
+        <p>
+          Hurray, you have already subscribed the service.
+        </p>
+      </section>
+
+      <footer>
+      <form action="index.php" >
+          <button type="submit" name="submit" >Home</button>
+        </form>
+      </footer>
+
+    </div>
+  </section>
+
+  
+
+</body >
+</html >
+
