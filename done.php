eeab6b20c64de86fb4a2e7edb675f50a2ecade29
shahr6485 <1903166.cse.cec@cgc.edu.in>
2022-05-14 14:26:31 +0530

    Create done.php

---
+++ done.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <!-- Required meta tags -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- CSS File -->
+  <link rel="stylesheet" href="style.css">
+  <title>Mail Verify</title>
+</head>
+
+<body>
+
+
+
+  <section class="inline">
+    <div class="content">
+      <header>
+        <h1>Mail verified!</h1>
+      </header>
+      <section>
+        <p>
+          Now enjoy the best of comic world at your digital devices in every 5 minutes.
+        </p>
+      </section>
+
+    </div>
+  </section>
+
+
+
+</body>
+
+</html>
