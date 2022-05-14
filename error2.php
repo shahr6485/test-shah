f245e832c4ff84f73dc01e76d1de7ca2a408559e
shahr6485 <1903166.cse.cec@cgc.edu.in>
2022-05-14 14:26:46 +0530

    Create error2.php

---
+++ error2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <!-- Required meta tags -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <!-- CSS File -->
+  <link rel="stylesheet" href="style.css">
+  <title>Mail Verify</title>
+</head>
+
+<body>
+
+  
+
+  <section class="inline">
+    <div class="content">
+      <header>
+        <h1>OOOPS!</h1>
+      </header>
+      <section>
+        <p>
+          Something went wrong, please check again.
+        </p>
+      </section>
+      
+    </div>
+  </section>
+
+ 
+
+</body >
+</html >
